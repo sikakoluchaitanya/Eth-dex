e20067dc1b90e8dea61aa55bb5414905227f0d14
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2024-11-10 19:31:14 +0530

    project setup

---
+++ utils/index.js
@@ -0,0 +1,6 @@
+export const shortAddress = (address) => `${address.slice(0, 6)}...${address?.slice(address.length-4)}`;
+
+export const parseErrorMsg = (e) => {
+    const json = JSON.parse(JSON.stringify(e));
+    return json?.reason || json?.error?.message;
+};
